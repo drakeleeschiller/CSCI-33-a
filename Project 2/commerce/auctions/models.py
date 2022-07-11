65ad1d2cdc5229f4410bb4bd255390bdeddd0447
Drake Schiller <dls396@cornell.edu>
2022-07-10 21:23:58 -0400

    reset database for models

--- Project 2/commerce/auctions/models.py
+++ Project 2/commerce/auctions/models.py
@@ -1,28 +1,39 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime, timedelta
 
 
 class User(AbstractUser):
     # TO-DO: Implement this class
-    def __str__(self):
-        return f"{self.get_full_name()}"
+    pass
 
-class Listing(models.Model):
+class Product(models.Model):
     # TO-DO: Implement this class
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=64)
-    start_bid = models.IntegerField()
-
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_listings")
 
     def __str__(self):
         return f"{self.title}: {self.description}"
 
+class Listing(models.Model):
+    # TO-DO: Implement this class
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    product_id = models.OneToOneField(Product, on_delete=models.CASCADE, primary_key=True)
+
+    current_bid = models.IntegerField()
+    num_bids = models.IntegerField()
+    active = models.BooleanField()
+    # # Duration in days
+    # duration = models.IntegerField()
+    # start_time = models.DateTimeField()
+    # end_time = start_time + timedelta(days = duration)
 
 class Bid(models.Model):
     # TO-DO: Implement this class
-    pass
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+
 
 class Comment(models.Model):
     # TO-DO: Implement this class
-    pass
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
