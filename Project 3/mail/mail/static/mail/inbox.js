f6c711fba2282cfb132785a7c4c2e9c69f8fa062
Drake Schiller <dls396@cornell.edu>
2022-07-17 13:40:34 -0700

    Add functionality to emails-view (display list of emails from an inbox on the inbox's page)

--- Project 3/mail/mail/static/mail/inbox.js
+++ Project 3/mail/mail/static/mail/inbox.js
@@ -16,10 +16,34 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+  const compose_recipients = document.querySelector('#compose-recipients');
+  const compose_subject = document.querySelector('#compose-subject');
+  const compose_body = document.querySelector('#compose-body');
+
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  compose_recipients.value = '';
+  compose_subject.value = '';
+  compose_body.value = '';
+
+  // Send the email out on submission of the form
+  document.querySelector('#compose-form').onsubmit = () => {
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: compose_recipients.value,
+          subject: compose_subject.value,
+          body: compose_body.value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    });
+    load_mailbox('sent');
+    // Prevent the actual submission of the form
+    return false;
+  }
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +54,29 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Iterate through the list of emails and put each one in a div
+  fetch_email_list(mailbox).then(result => {
+    display_email_list(result)
+  })
 }
+
+// Fetch the list of emails from a specific mailbox
+// Returns an array of dictionaries
+async function fetch_email_list(mailbox) {
+  const response = await fetch('/emails/' + mailbox, {
+    method: "GET"
+  });
+  return await response.json();
+}
+
+// Create divs for and display each email in a list
+function display_email_list(email_list) {
+  return email_list.forEach(email => {
+    // TO-DO: Might want to change this later to a custom element
+    const element = document.createElement('div');
+    element.innerHTML = email['subject'];
+    document.querySelector('#emails-view').append(element);
+  });
+}
+
