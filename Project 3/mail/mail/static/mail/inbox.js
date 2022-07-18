7eff578d69b96286d28d7013252803bedb4dd95f
Drake Schiller <dls396@cornell.edu>
2022-07-17 17:26:11 -0700

    Change read status and bg color to grey if read

--- Project 3/mail/mail/static/mail/inbox.js
+++ Project 3/mail/mail/static/mail/inbox.js
@@ -15,6 +15,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#message-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   const compose_recipients = document.querySelector('#compose-recipients');
@@ -51,6 +52,7 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#message-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
@@ -58,9 +60,11 @@ function load_mailbox(mailbox) {
 
   // Iterate through the list of emails and put each one in a div
   fetch_email_list(mailbox).then(result => {
-    display_email_list(result)
+    display_email_list(result, mailbox)
   })
 }
+
+
 
 // Fetch the list of emails from a specific mailbox
 // Returns an array of dictionaries
@@ -72,17 +76,62 @@ async function fetch_email_list(mailbox) {
 }
 
 // Create divs for and display each email in a list
-function display_email_list(email_list) {
+function display_email_list(email_list, mailbox) {
   return email_list.forEach(email => {
-    // TO-DO: Might want to change this later to a custom element
-    const element = document.createElement('a');
-    const style_list = ["list-group-item", "list-group-item-action", "flex-column", "align-items-start"];
-    element.classList.add(...style_list);
-    // TO-DO: Change this to correct href once view for a single email is created
-    element.href = '#'
-    element.innerHTML = email['subject'];
-    document.querySelector('#emails-view').append(element);
+    console.log("HAS THE EMAIL BEEN READ?:", email["read"])
+    format_one_row(email, mailbox)
   });
 }
 
+function format_one_row(email, mailbox) {
+  console.log("MAILBOX IS:", mailbox);
+  // Create the click-able parent box
+  const element = document.createElement('a');
 
+  // Add an event-listener if the row has been clicked, changing 'read' status to true
+  element.addEventListener('click', () => {
+    // Immediately after clicking, set the bg color to grey
+    if (mailbox != 'sent'){
+      element.style.backgroundColor = "rgb(205, 199, 199)";
+    }
+    fetch(`/emails/${email['id']}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          read: true
+      })
+    });
+  })
+
+  // Styling
+  const element_list = ["list-group-item", "list-group-item-action", "flex-column", "align-items-start"];
+  // When rendering, set the background color to grey if the email has been read
+  if (email['read'] && mailbox != 'sent') {
+    element.classList.add("read")
+  }
+  element.classList.add(...element_list);
+  // TO-DO: Change this to correct href once view for a single email is created
+  element.href = '#'
+
+
+
+  // Create and format the row's content
+  const row_content = document.createElement('div');
+  row_content_style_list = ["d-flex", "w-100", "justify-content-between"]
+  row_content.classList.add(...row_content_style_list)
+  
+
+  let email_person = (mailbox == 'sent' ? `To: ${email['recipients']}` : `From: ${email['sender']}`);
+
+  // Nice to have: Change the sender to be the name of the sender, not their email
+  row_content.innerHTML = `
+    <div>
+      <h5 class="mb-1">${email['subject']}</h5>
+      <h6>${email_person}</h6>
+    </div>
+    <small>${email['timestamp']}</small>
+    `;
+  
+  // Add
+  element.append(row_content);
+  document.querySelector('#emails-view').append(element);
+}
